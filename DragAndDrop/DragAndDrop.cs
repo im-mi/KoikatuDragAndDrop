972122afc590404efdf280723d7102bb835cc574
im-mi <im.mi.mail.mi@gmail.com>
2018-04-24 20:13:11 -0400

    Initial commit

---
+++ DragAndDrop/DragAndDrop.cs
@@ -0,0 +1,59 @@
+﻿using B83.Win32;
+using BepInEx;
+using ChaCustom;
+using System;
+using System.Collections.Generic;
+
+namespace DragAndDrop
+{
+    [BepInPlugin(GUID: "com.immi.koikatu.draganddrop", Name: "Drag and Drop", Version: "1.0.0")]
+    class DragAndDrop : BaseUnityPlugin
+    {
+        UnityDragAndDropHook hook;
+
+        private void OnEnable()
+        {
+            hook = new UnityDragAndDropHook();
+            hook.InstallHook();
+            hook.OnDroppedFiles += OnDroppedFiles;
+        }
+
+        private void OnDisable()
+        {
+            hook.UninstallHook();
+        }
+
+        private void OnDroppedFiles(List<string> aFiles, POINT aPos)
+        {
+            if (aFiles.Count == 0) return;
+            var path = aFiles[0];
+            if (path == null) return;
+
+            try
+            {
+                LoadCharacter(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error when loading character: {ex}");
+            }
+        }
+
+        private void LoadCharacter(string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            var chaCtrl = Singleton<CustomBase>.Instance.chaCtrl;
+            var chaFile = chaCtrl.chaFile;
+            bool flag6 = true; // face
+            bool flag7 = true; // body
+            bool flag8 = true; // hair
+            bool parameter = true; // character
+            bool flag9 = true; // coordinates
+            chaFile.LoadFileLimited(path, chaCtrl.sex, flag6, flag7, flag8, parameter, flag9);
+            chaCtrl.ChangeCoordinateType(true);
+            chaCtrl.Reload(!flag9, !flag6 && !flag9, !flag8, !flag7);
+            Singleton<CustomBase>.Instance.updateCustomUI = true;
+            Singleton<CustomHistory>.Instance.Add5(chaCtrl, chaCtrl.Reload, !flag9, !flag6 && !flag9, !flag8, !flag7);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Koikatu Drag and Drop
+Adds drag and drop support for Koikatu, making it possible to load character cards by dragging them into the window from Explorer.
+
+# Limitations
+- Only supports dragging from Explorer
+- Doesn't support coordinate cards
+- Performs no sanity checks
+  - For example, you can load characters of the wrong sex, but the original sex will be retained even after saving.
