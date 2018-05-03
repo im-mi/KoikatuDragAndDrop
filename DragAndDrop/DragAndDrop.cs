8eccb0ae1b03d3e9831107c5f059de5f1b1f1899
im-mi <im.mi.mail.mi@gmail.com>
2018-05-02 23:51:19 -0400

    Improved character loading code and added proper logging

--- DragAndDrop/DragAndDrop.cs
+++ DragAndDrop/DragAndDrop.cs
@@ -4,6 +4,7 @@ using ChaCustom;
 using Illusion.Game;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace DragAndDrop
 {
@@ -30,35 +31,35 @@ namespace DragAndDrop
             var path = aFiles[0];
             if (path == null) return;
 
-            try
+            if (Singleton<CustomBase>.IsInstance())
             {
-                LoadCharacter(path);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error when loading character: {ex}");
+                try
+                {
+                    LoadCharacter(path);
+                    Utils.Sound.Play(SystemSE.ok_s);
+                }
+                catch (Exception ex)
+                {
+                    BepInLogger.Log($"Character load failed", true);
+                    BepInLogger.Log(ex.ToString());
+                    Utils.Sound.Play(SystemSE.ok_l);
+                }
             }
         }
 
         private void LoadCharacter(string path)
         {
             if (path == null) throw new ArgumentNullException(nameof(path));
-            bool loadFaceEnabled = true;
-            bool loadBodyEnabled = true;
-            bool loadHairEnabled = true;
-            bool loadCharacterEnabled = true;
-            bool loadCoordinateEnabled = true;
-            Utils.Sound.Play(SystemSE.ok_s);
+
             var chaCtrl = Singleton<CustomBase>.Instance.chaCtrl;
             var chaFile = chaCtrl.chaFile;
-            chaFile.LoadFileLimited(path, chaCtrl.sex,
-                loadFaceEnabled, loadBodyEnabled, loadHairEnabled, loadCharacterEnabled, loadCoordinateEnabled);
+
+            if (!chaFile.LoadCharaFile(path, chaCtrl.sex))
+                throw new IOException();
             chaCtrl.ChangeCoordinateType(true);
-            chaCtrl.Reload(!loadCoordinateEnabled,
-                !loadFaceEnabled && !loadCoordinateEnabled, !loadHairEnabled, !loadBodyEnabled);
+            chaCtrl.Reload();
             Singleton<CustomBase>.Instance.updateCustomUI = true;
-            Singleton<CustomHistory>.Instance.Add5(chaCtrl, chaCtrl.Reload,
-                !loadCoordinateEnabled, !loadFaceEnabled && !loadCoordinateEnabled, !loadHairEnabled, !loadBodyEnabled);
+            Singleton<CustomHistory>.Instance.Add5(chaCtrl, chaCtrl.Reload, false, false, false, false);
         }
     }
 }
