d6cc2ff82b69f2af7195416626a9c95ecf7e751c
ManlyMarco <dummyaccx@gmail.com>
2018-10-05 19:35:22 +0200

    Code cleanup, additional logging

--- DragAndDrop/DragAndDrop.cs
+++ DragAndDrop/DragAndDrop.cs
@@ -16,8 +16,8 @@ namespace DragAndDrop
     [BepInPlugin("com.immi.koikatu.draganddrop", "Drag and Drop", "1.2")]
     internal class DragAndDrop : BaseUnityPlugin
     {
-        private const string charaToken = "【KoiKatuChara】";
-        private const string studioToken = "【KStudio】";
+        private const string CharaToken = "【KoiKatuChara】";
+        private const string StudioToken = "【KStudio】";
 
         private UnityDragAndDropHook _hook;
 
@@ -40,47 +40,63 @@ namespace DragAndDrop
             var path = aFiles[0];
             if (path == null)
                 return;
+
+            if (!Singleton<Scene>.IsInstance()) return;
+
             var extension = Path.GetExtension(path).ToLower();
             if (extension != ".png")
             {
-                Logger.Log(LogLevel.Message, $"Unsupported file type {extension}. Cards are only in .png format!");
+                Logger.Log(LogLevel.Message, $"Unsupported file type {extension}. Only .png files are supported!");
                 return;
             }
 
-            if (Singleton<Scene>.IsInstance())
-                try
+            try
+            {
+                var pngType = CheckPngType(path);
+
+                if (pngType == PngType.Unknown)
                 {
-                    var pngType = CheckPngType(path);
-                    if (Singleton<Scene>.Instance.NowSceneNames.Any(sceneName => sceneName == "CustomScene"))
+                    Logger.Log(LogLevel.Message, "Unknown file format, can't load");
+                    Utils.Sound.Play(SystemSE.ok_l);
+                    return;
+                }
+
+                if (Singleton<Scene>.Instance.NowSceneNames.Any(sceneName => sceneName == "CustomScene"))
+                {
+                    if (Singleton<CustomBase>.IsInstance())
                     {
-                        if (Singleton<CustomBase>.IsInstance() && pngType == PngType.KoikatuChara)
+                        if (pngType == PngType.KoikatuChara)
                         {
                             LoadCharacter(path);
                             Utils.Sound.Play(SystemSE.ok_s);
                         }
-                    }
-                    else if (Singleton<Scene>.Instance.NowSceneNames.Any(sceneName => sceneName == "Studio"))
-                    {
-                        if (pngType == PngType.KoikatuChara)
-                        {
-                            AddChara(path);
-                            Utils.Sound.Play(SystemSE.ok_s);
-                        }
                         else if (pngType == PngType.KStudio)
                         {
-                            LoadScene(path);
-                            Utils.Sound.Play(SystemSE.ok_s);
+                            Logger.Log(LogLevel.Message, "Can't load scene files in character maker");
+                            Utils.Sound.Play(SystemSE.ok_l);
                         }
                     }
-                    if (pngType == PngType.Unknown)
-                        Utils.Sound.Play(SystemSE.ok_l);
                 }
-                catch (Exception ex)
+                else if (Singleton<Scene>.Instance.NowSceneNames.Any(sceneName => sceneName == "Studio"))
                 {
-                    Logger.Log(LogLevel.Message, $"Character load failed - {ex.Message}");
-                    Logger.Log(LogLevel.Error, $"[DragAndDrop] {ex}");
-                    Utils.Sound.Play(SystemSE.ok_l);
+                    if (pngType == PngType.KoikatuChara)
+                    {
+                        AddChara(path);
+                        Utils.Sound.Play(SystemSE.ok_s);
+                    }
+                    else if (pngType == PngType.KStudio)
+                    {
+                        LoadScene(path);
+                        Utils.Sound.Play(SystemSE.ok_s);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(LogLevel.Message, $"Character load failed - {ex.Message}");
+                Logger.Log(LogLevel.Error, $"[DragAndDrop] {ex}");
+                Utils.Sound.Play(SystemSE.ok_l);
+            }
         }
 
         private void LoadScene(string path)
@@ -89,73 +105,70 @@ namespace DragAndDrop
                 throw new ArgumentNullException(nameof(path));
             StartCoroutine(Singleton<Studio.Studio>.Instance.LoadSceneCoroutine(path));
         }
-        
+
         private static void AddChara(string path)
         {
             var charaCtrl = new ChaFileControl();
-            if (charaCtrl.LoadCharaFile(path, 1, true, true))
+            if (!charaCtrl.LoadCharaFile(path, 1, true, true)) return;
+
+            var ocichar = Studio.Studio.GetCtrlInfo(Singleton<Studio.Studio>.Instance.treeNodeCtrl.selectNode) as OCIChar;
+            if (ocichar != null && charaCtrl.parameter.sex == ocichar.sex)
             {
-                var ocichar = Studio.Studio.GetCtrlInfo(Singleton<Studio.Studio>.Instance.treeNodeCtrl.selectNode) as OCIChar;
-                if (ocichar != null && charaCtrl.parameter.sex == ocichar.sex)
+                var array = (from v in Singleton<GuideObjectManager>.Instance.selectObjectKey
+                    select Studio.Studio.GetCtrlInfo(v) as OCIChar
+                    into v
+                    where v != null
+                    where v.oiCharInfo.sex == (int) charaCtrl.parameter.sex
+                    select v).ToArray();
+                var i = 0;
+                var num = array.Length;
+                while (i < num)
                 {
-                    var array = (from v in Singleton<GuideObjectManager>.Instance.selectObjectKey
-                        select Studio.Studio.GetCtrlInfo(v) as OCIChar
-                        into v
-                        where v != null
-                        where v.oiCharInfo.sex == (int) charaCtrl.parameter.sex
-                        select v).ToArray();
-                    var i = 0;
-                    var num = array.Length;
-                    while (i < num)
-                    {
-                        array[i].ChangeChara(path);
-                        i++;
-                    }
-                    return;
+                    array[i].ChangeChara(path);
+                    i++;
                 }
-                if (charaCtrl.parameter.sex == 0)
-                {
-                    Singleton<Studio.Studio>.Instance.AddMale(path);
-                    return;
-                }
-                if (charaCtrl.parameter.sex == 1)
-                    Singleton<Studio.Studio>.Instance.AddFemale(path);
+                return;
             }
+            if (charaCtrl.parameter.sex == 0)
+            {
+                Singleton<Studio.Studio>.Instance.AddMale(path);
+                return;
+            }
+            if (charaCtrl.parameter.sex == 1)
+                Singleton<Studio.Studio>.Instance.AddFemale(path);
         }
 
         private static PngType CheckPngType(string path)
         {
             using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var binaryReader = new BinaryReader(fileStream))
             {
-                using (var binaryReader = new BinaryReader(fileStream))
+                try
                 {
-                    try
-                    {
-                        PngFile.SkipPng(binaryReader);
-                        binaryReader.ReadInt32();
-                    }
-                    catch (EndOfStreamException)
-                    {
-                        return PngType.Unknown;
-                    }
-                    try
-                    {
-                        if (binaryReader.ReadString() == "【KoiKatuChara】")
-                            return PngType.KoikatuChara;
-                    }
-                    catch (EndOfStreamException)
-                    {
-                    }
-                    var byteCount = Encoding.UTF8.GetByteCount("【KStudio】");
-                    binaryReader.BaseStream.Seek(-(long) byteCount, SeekOrigin.End);
-                    try
-                    {
-                        if (Encoding.UTF8.GetString(binaryReader.ReadBytes(byteCount)) == "【KStudio】")
-                            return PngType.KStudio;
-                    }
-                    catch (EndOfStreamException)
-                    {
-                    }
+                    PngFile.SkipPng(binaryReader);
+                    binaryReader.ReadInt32();
+                }
+                catch (EndOfStreamException)
+                {
+                    return PngType.Unknown;
+                }
+                try
+                {
+                    if (binaryReader.ReadString() == CharaToken)
+                        return PngType.KoikatuChara;
+                }
+                catch (EndOfStreamException)
+                {
+                }
+                var byteCount = Encoding.UTF8.GetByteCount(StudioToken);
+                binaryReader.BaseStream.Seek(-(long) byteCount, SeekOrigin.End);
+                try
+                {
+                    if (Encoding.UTF8.GetString(binaryReader.ReadBytes(byteCount)) == StudioToken)
+                        return PngType.KStudio;
+                }
+                catch (EndOfStreamException)
+                {
                 }
             }
             return PngType.Unknown;
@@ -181,7 +194,7 @@ namespace DragAndDrop
             Singleton<CustomBase>.Instance.updateCustomUI = true;
             Singleton<CustomHistory>.Instance.Add5(chaCtrl, chaCtrl.Reload, false, false, false, false);
         }
-        
+
         private enum PngType
         {
             Unknown,
